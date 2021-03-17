5b3778afb556804bd3d97ca2f057f882bba50da6
Sotheanith Sok <sotheanith@users.noreply.github.com>
2021-03-16 17:26:25 -0700

    Add RDFS Vocabulary (#2)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from Backend.rdf import RDF
-rdf = RDF()
+from Backend.rdf import RDF_Graph
+rdf = RDF_Graph()
 
 
 #https://data.lacity.org/resource/amvf-fr72
 #https://data.lacity.org/resource/2nrs-mtv8
 
-rdf.export("output.txt",'csv')
+print(rdf.export("output.txt"))
